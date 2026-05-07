2f196e68405034a6d06355df3c195cd65132536b
valtrixAI <charlyvangrim@gmail.com>
2026-05-07 13:48:26 +0200

    Update debug.js

--- api/debug.js
+++ api/debug.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.status(200).json({
     has_URL: !!process.env.SUPABASE_URL,
     url_start: process.env.SUPABASE_URL?.substring(0, 20) || null,
